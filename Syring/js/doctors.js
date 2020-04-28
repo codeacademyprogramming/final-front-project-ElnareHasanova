f6646f2946b6559e70570f45ef4ea20d25bb79a9
ElnareHasanova <elnarehasanova14@gmail.com>
2020-04-28 15:47:51 +0400

    doctors add modal

---
+++ Syring/js/doctors.js
@@ -0,0 +1,18 @@
+'use strict';
+$(function () {
+    $(document).on('click', '.appoinment-form', function () {
+        $('.appointment-modal').show();
+ 
+        $(document).on('click', '.appointment-modal', function () {
+        $('.appointment-modal').hide();
+            
+        })
+    })
+ 
+ 
+ 
+ 
+ 
+})
+
+
